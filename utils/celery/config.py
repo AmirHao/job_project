42976d8b1dfdfa667b37110c556fe761fe8f1ba9
hzm <haozemin@shun178.com>
2021-05-24 15:13:10 +0800

    210524[optimize]celery配置扩展

--- utils/celery/config.py
+++ utils/celery/config.py
@@ -22,3 +22,14 @@ CELERY_ENABLE_UTC = True
 # CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24  # 任务过期时间
 #
 # CELERY_ACCEPT_CONTENT = ['json', 'msgpack']  # 指定接受的内容类型
+
+# 有些情况可以防止死锁
+CELERYD_FORCE_EXECV = True
+# 设置并发worker数量
+CELERYD_CONCURRENCY = 20
+# 允许重试
+CELERY_ACKS_LATE = True
+# 每个worker最多执行100个任务被销毁，可以防止内存泄漏
+CELERYD_MAX_TASKS_PER_CHILD = 100
+# 超时时间
+CELERYD_TASK_TIME_LIMIT = 12 * 30
