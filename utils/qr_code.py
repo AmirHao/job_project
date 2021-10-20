cfe5f6fae7e19e3f8208537d2c9a94818cbf0aeb
hzm <haozemin@shun178.com>
2021-10-20 18:04:08 +0800

    211020[utils.qr_code]生成二维码

---
+++ utils/qr_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ = hzm
+# __date__ = 2021/10/20
+import qrcode
+
+
+def creats_qr_code():
+    # 调用qrcode的make()方法传入url或者想要展示的内容
+    img = qrcode.make('https://www.baidu.com')
+    # 保存
+    img.save("ignore_data/二维码.png")
+
+
+def create_qr_code_2_file():
+    img = qrcode.make('http://www.douban.com')
+    # 写入文件
+    with open('ignore_data/test1.png', 'wb') as f:
+        img.save(f)
