afee3d47af229c86a93fd2c4c1bee2cef51a7a6f
hzm <haozemin@shun178.com>
2021-11-25 15:02:42 +0800

    format code

--- utils/qr_code.py
+++ utils/qr_code.py
@@ -10,15 +10,15 @@ from PIL import Image
 
 def creats_qr_code():
     # 调用qrcode的make()方法传入url或者想要展示的内容
-    img = qrcode.make('https://www.baidu.com')
+    img = qrcode.make("https://www.baidu.com")
     # 保存
     img.save("ignore_data/二维码.png")
 
 
 def create_qr_code_2_file():
-    img = qrcode.make('http://www.douban.com')
+    img = qrcode.make("http://www.douban.com")
     # 写入文件
-    with open('ignore_data/test1.png', 'wb') as f:
+    with open("ignore_data/test1.png", "wb") as f:
         img.save(f)
 
 
