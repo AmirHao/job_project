d8b3697cb18819d02b633062c018efd750a59a2a
hzmin <971029952@qq.com>
2021-12-29 11:31:02 +0800

    [1229][settings]修改本地数据库

--- settings/hzm.py
+++ settings/hzm.py
@@ -12,7 +12,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'my_project',
         'USER': 'root',
-        'PASSWORD': '12345678',
+        'PASSWORD': 'PassWord',
         'HOST': 'localhost',
         'PORT': '3306',
         'TEST': {
